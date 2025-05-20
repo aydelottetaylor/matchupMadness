7e22edf5854bd30c0f48061d99ebab164eee4696
Taylor Aydelotte <tayloraydelotte0@gmail.com>
2025-05-20 15:16:00 -0600

    Safe Config
    Made a new safe config file

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+db_config = {
+    'host': os.environ.get('DB_HOST'),
+    'user': os.environ.get('DB_USER'),
+    'password': os.environ.get('DB_PASSWORD'),
+    'database': os.environ.get('DB_NAME'),
+    'use_pure': True
+}
