b1d229308c92571d4d9e073c9af2f10b43eb22a0
Taylor Aydelotte <tayloraydelotte0@gmail.com>
2025-05-20 15:09:10 -0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-db_config = {
-    "host": "admin.cjq0cgqg0p8v.us-east-2.rds.amazonaws.com",
-    "user": "admin",
-    "password": "MatchupMadness2025",
-    "database": "matchupMadness",
-    "use_pure": True
-}
